fb62ef1a3eaa45691d882bd14e0f5544676328e0
mhogg <mhogg@student.21-school.ru>
2022-03-21 10:34:09 +0500

    add tests

---
+++ tests/sources/vector_tests/insert(range).cpp
@@ -0,0 +1,37 @@
+#include "../system/system_methods.ipp"
+#include "__service.ipp"
+
+template <typename T>
+std::vector<int> insert_test_3(std::vector<T> vector) {
+    std::vector<int> v;
+    std::vector<int> tmp;
+    tmp.assign(2600 * _ratio, 1);
+    vector.assign(4200 * _ratio, 1);
+    g_start1 = timer();
+    vector.insert(vector.end() - 1000 * _ratio, tmp.begin(), tmp.end());
+    g_end1 = timer();
+    v.push_back(vector[3]);
+    v.push_back(vector.size());
+    v.push_back(vector.capacity());
+    return v;
+}
+
+template <typename T>
+std::vector<int> insert_test_3(_vector<T> vector) {
+    std::vector<int> v;
+    _vector<int> tmp;
+    tmp.assign(2600 * _ratio, 1);
+    vector.assign(4200 * _ratio, 1);
+    g_start2 = timer();
+    vector.insert(vector.end() - 1000 * _ratio, tmp.begin(), tmp.end());
+    g_end2 = timer();
+    v.push_back(vector[3]);
+    v.push_back(vector.size());
+    v.push_back(vector.capacity());
+    return v;
+}
+
+int main() {
+
+    exit(run_vector_unit_test<int>("insert(range)", insert_test_3, insert_test_3));
+}
